f8d6522a3a4c4fa04910777797b60de18dafa880
shubham modi <shubhammodigzb@gmail.com>
2020-06-30 19:13:15 +0000

    shaayad final

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var commentRoutes     =require("./routes/comments"),
 	campgroundRoutes  =require("./routes/campgrounds"),
 	indexRoutes       =require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://dbuser:papamummy@2532314@yelpcamp.ztinz.mongodb.net/<dbname>?retryWrites=true&w=majority",{useNewUrlParser: true});
+// "mongodb://localhost:27017/yelp_camp"
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
@@ -48,4 +49,6 @@ app.use("/campgrounds/:id/comments",commentRoutes);
 app.use("/", indexRoutes);
 
 
-app.listen(process.env.PORT,process.env.IP);
+app.listen(3000,function(req,res){
+	console.log("ALL IS WELL");
+});
