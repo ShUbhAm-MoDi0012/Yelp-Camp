3117e5ca5c6990bd73b11bad30e3d26d9c4a236f
shubham modi <shubhammodigzb@gmail.com>
2020-07-01 11:51:13 +0000

    asdf

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var commentRoutes     =require("./routes/comments"),
 	campgroundRoutes  =require("./routes/campgrounds"),
 	indexRoutes       =require("./routes/index");
 
-mongoose.connect("mongodb+srv://dbuser:papamummy@2532314@yelpcamp.ztinz.mongodb.net/dbuser?retryWrites=true&w=majority",{useNewUrlParser: true,useCreateIndex: true});
+mongoose.connect(process.env.DATABASEURL,{useNewUrlParser: true,useCreateIndex: true});
 // "mongodb://localhost:27017/yelp_camp"
+// "mongodb+srv://dbuser:papamummy@2532314@yelpcamp.ztinz.mongodb.net/dbuser?retryWrites=true&w=majority"
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
